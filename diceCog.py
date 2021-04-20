17cdf9ec00adcaf644df32bc21b9185481d02bb6
XShadeSlayerXx <xshadeslayerxx@gmail.com>
2021-04-19 21:42:18 -0500

    dice cog now recognizes additions to rolls

--- diceCog.py
+++ diceCog.py
@@ -22,17 +22,27 @@ class Dice(commands.Cog):
     )
     async def rollDice(self, ctx, *, mc = ''):
         mc = mc.lower()
+        if '+' in mc:
+            place = mc.find('+')
+            add = int(mc[1 + place:])
+        else:
+            add = None
         if mc != '':
             # are we rolling random dice?
             if re.search(diceSplit, mc) is not None:
                 dice = mc.split('d')
                 if dice[0] == '':
                     dice[0] = 1
+                if add is not None:
+                    place = dice[1].find('+')
+                    dice[1] = dice[1][:place]
                 dice[0] = sorted((1, int(dice[0]), maxDice))[1]
                 dice[1] = sorted((2, int(dice[1]), maxPips))[1]
                 total = [random.randrange(int(dice[1])) + 1 for _ in range(int(dice[0]))]
                 rolled = f'{dice[0]}d{dice[1]}'
-                msg = f'{rolled} -- {", ".join([str(x) for x in total])}\n'
+                msg = f'{rolled} -- {", ".join([str(x) for x in total])}'
+                if add is not None:
+                    msg += f' + {add} = {sum(total) + add}'
                 await ctx.send(msg)
         else:
             ran = random.randrange(6) + 1
