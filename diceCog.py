95a51a93664444107e377c0079525e6fdf890a5d
XShadeSlayerXx <xshadeslayerxx@gmail.com>
2022-09-08 00:38:04 -0500

    updated to work with discord py v2

--- diceCog.py
+++ diceCog.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
-from dislash import slash_command, ActionRow, Button, ButtonStyle, ResponseType, Option, OptionType
+from discord import app_commands, ButtonStyle, Member, Interaction
+from discord.ui import button, Button, View
+from discord.app_commands import Choice
 
 import random
 import re
@@ -10,63 +12,92 @@ maxPips = 100
 
 diceSplit = re.compile(r'\d*d\d+')
 
+async def dice_backend(ctx, *, mc = ''):
+    name = ctx.author.display_name
+    msg = f'{name} rolled '
+    mc = mc.lower()
+    if '+' in mc:
+        place = mc.find('+')
+        add = int(mc[1 + place:])
+    else:
+        add = None
+    if mc != '':
+        # are we rolling random dice?
+        if re.search(diceSplit, mc) is not None:
+            dice = mc.split('d')
+            if dice[0] == '':
+                dice[0] = 1
+            if add is not None:
+                place = dice[1].find('+')
+                dice[1] = dice[1][:place]
+            dice[0] = sorted((1, int(dice[0]), maxDice))[1]
+            dice[1] = sorted((2, int(dice[1]), maxPips))[1]
+            total = [random.randrange(int(dice[1])) + 1 for _ in range(int(dice[0]))]
+            rolled = f'{dice[0]}d{dice[1]}'
+            numSuccesses = None
+            if dice[1] == 6 and add is None:
+                msg += f'{rolled} -- '
+                numSuccesses = 0
+                for x in total:
+                    if x < 4:  #don't bold failed rolls
+                        msg += str(x)
+                    else:  #bold successes
+                        msg += f'**{str(x)}**'
+                        numSuccesses += 1
+                    msg += ', '
+                msg = msg[:-2]
+            else:
+                msg += f'{rolled} -- {", ".join([str(x) for x in total])}'
+            if add is not None:
+                msg += f' + {add} = {sum(total) + add}'
+            if numSuccesses is not None:
+                msg += (f'\n{numSuccesses} Successes' if numSuccesses != 1 else f'\n{numSuccesses} Success')
+            return msg
+        else:
+            try:
+                int(mc)
+                await dice_backend(ctx, mc = f'{mc}d6')
+            except:
+                pass
+    else:
+        ran = random.randrange(6) + 1
+        return msg + f'a {ran}'
+
+async def dice_backend_slash(sides, dice, addition):
+    msg = ''
+    if sides or dice or addition:
+        # are we rolling random dice?
+        total = [random.randrange(int(sides)) + 1 for _ in range(int(dice))]
+        rolled = f'{dice}d{sides}'
+        numSuccesses = None
+        if sides == 6 and addition is None:
+            msg += f'{rolled} -- '
+            numSuccesses = 0
+            for x in total:
+                if x < 4: #don't bold failed rolls
+                    msg += str(x)
+                else: #bold successes
+                    msg += f'**{str(x)}**'
+                    numSuccesses += 1
+                msg += ', '
+            msg = msg[:-2] #remove trailing ', '
+        else:
+            msg += f'{rolled} -- {", ".join([str(x) for x in total])}'
+        if addition is not None:
+            msg += f' + {addition} = {sum(total) + addition}'
+        if numSuccesses is not None:
+            msg += (f'\n{numSuccesses} Successes' if numSuccesses != 1 else f'\n{numSuccesses} Success')
+        return msg
+    else:
+        ran = random.randrange(6) + 1
+        return msg + f'a {ran}'
+
+
 class Dice(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.timeout = 60 * 2
-
-    async def dice_backend(self, ctx, *, mc = ''):
-        name = ctx.author.display_name
-        msg = f'{name} rolled '
-        mc = mc.lower()
-        if '+' in mc:
-            place = mc.find('+')
-            add = int(mc[1 + place:])
-        else:
-            add = None
-        if mc != '':
-            # are we rolling random dice?
-            if re.search(diceSplit, mc) is not None:
-                dice = mc.split('d')
-                if dice[0] == '':
-                    dice[0] = 1
-                if add is not None:
-                    place = dice[1].find('+')
-                    dice[1] = dice[1][:place]
-                dice[0] = sorted((1, int(dice[0]), maxDice))[1]
-                dice[1] = sorted((2, int(dice[1]), maxPips))[1]
-                total = [random.randrange(int(dice[1])) + 1 for _ in range(int(dice[0]))]
-                rolled = f'{dice[0]}d{dice[1]}'
-                numSuccesses = None
-                if dice[1] == 6 and add is None:
-                    msg += f'{rolled} -- '
-                    numSuccesses = 0
-                    for x in total:
-                        if x < 4: #don't bold failed rolls
-                            msg += str(x)
-                        else: #bold successes
-                            msg += f'**{str(x)}**'
-                            numSuccesses += 1
-                        msg += ', '
-                    msg = msg[:-2]
-                else:
-                    msg += f'{rolled} -- {", ".join([str(x) for x in total])}'
-                if add is not None:
-                    msg += f' + {add} = {sum(total) + add}'
-                if numSuccesses is not None:
-                    msg += (f'\n{numSuccesses} Successes' if numSuccesses != 1 else f'\n{numSuccesses} Success')
-                return msg
-            else:
-                try:
-                    int(mc)
-                    await self.dice_backend(ctx, mc = f'{mc}d6')
-                except:
-                    pass
-        else:
-            ran = random.randrange(6) + 1
-            return msg + f'a {ran}'
-
 
     @commands.command(
         name = 'roll',
@@ -93,131 +124,72 @@ class Dice(commands.Cog):
                 note = f'*{note}*\n'
                 await self.rollDice(ctx = ctx, mc = mc, note = note)
                 return
-        msg = await self.dice_backend(ctx = ctx, mc = mc)
+        msg = await dice_backend(ctx = ctx, mc = mc)
 
-        button = [ActionRow(Button(
-            style = ButtonStyle.gray,
-            label = 'Roll Again',
-            custom_id = 'reroll'
-        ))]
-        final = note + msg
-        sent = await ctx.send(final, components = button)
+        buttons = RerollButton(timeout = self.timeout,
+                               note = note, mc = mc)
+        await ctx.send(msg, view = buttons)
 
-        on_click = sent.create_click_listener(timeout = self.timeout)
-        button_owner = ctx.author.id
 
-        @on_click.matching_id('reroll')
-        async def on_matching_reroll(inter):
-            if inter.author.id == button_owner:
-                first = inter.message.content.index('\n')+1 if note else 0
-                if inter.message.content.count('\n') > 6:
-                    first = inter.message.content.index('\n', first)
-                    final = inter.message.content[inter.message.content.index('\n', first+1)+1:]
-                else:
-                    if note:
-                        final = inter.message.content[first:]
-                    else:
-                        final = inter.message.content
-                extra = await self.dice_backend(ctx = ctx, mc = mc)
-                final = note + final + '\n' + extra
-                await inter.reply(content = final, type = ResponseType.UpdateMessage)
-            else:
-                await inter.create_response(content = 'The Reroll button is reserved '
-                                                      'for whoever ran the original message.',
-                                            ephemeral = True)
-
-        @on_click.timeout
-        async def on_timeout():
-            await sent.edit(components = [])
-
-    async def dice_backend_slash(self, sides, dice, addition):
-        msg = ''
-        if sides or dice or addition:
-            # are we rolling random dice?
-            total = [random.randrange(int(sides)) + 1 for _ in range(int(dice))]
-            rolled = f'{dice}d{sides}'
-            numSuccesses = None
-            if sides == 6 and addition is None:
-                msg += f'{rolled} -- '
-                numSuccesses = 0
-                for x in total:
-                    if x < 4: #don't bold failed rolls
-                        msg += str(x)
-                    else: #bold successes
-                        msg += f'**{str(x)}**'
-                        numSuccesses += 1
-                    msg += ', '
-                msg = msg[:-2] #remove trailing ', '
-            else:
-                msg += f'{rolled} -- {", ".join([str(x) for x in total])}'
-            if addition is not None:
-                msg += f' + {addition} = {sum(total) + addition}'
-            if numSuccesses is not None:
-                msg += (f'\n{numSuccesses} Successes' if numSuccesses != 1 else f'\n{numSuccesses} Success')
-            return msg
-        else:
-            ran = random.randrange(6) + 1
-            return msg + f'a {ran}'
-
-    @slash_command(
+    @app_commands.command(
         name = 'roll',
-        description = "Roll a single d6 by default. You can change the number of dice and/or the number of sides",
-        options = [
-            Option('dice', 'Number of dice to roll (up to 100)', OptionType.INTEGER),
-            Option('sides', 'Number of sides each die has (up to 100)', OptionType.INTEGER),
-            Option('flat_addition', 'Flat number to the roll', OptionType.INTEGER),
-            Option('note', 'Add a note for future reference', OptionType.STRING),
-            Option('private', 'Send a private message to you in this chat? (default: False)', OptionType.BOOLEAN)
-        ]
+        description = "Roll a single d6 by default. You can change the number of dice and/or the number of sides"
     )
-    async def rollDice_slash(self, inter, sides : int = 6, dice : int = 1,
-                             flat_addition : int = None, note : str = '', private : bool = False):
+    @app_commands.describe(
+        dice = "Number of dice to roll (up to 100)",
+        sides = "Number of sides each die has (up to 100)",
+        flat_addition = "Add a flat number to the roll",
+        note = "Add a note for future reference",
+        private = "Send a private message to you in this chat? (default: False)"
+    )
+    @app_commands.choices(
+        private = [
+            Choice(name = 'Yes', value = 1),
+            Choice(name = 'No', value = 0),
+        ],
+    )
+    async def rollDice_slash(self, inter,
+                             sides : app_commands.Range[int, 0, 100] = 6,
+                             dice : app_commands.Range[int, 0, 100] = 1,
+                             flat_addition : int = None,
+                             note : str = '',
+                             private : int = False):
+        private = bool(private)
         dice = sorted((1, dice, maxDice))[1]
         sides = sorted((2, sides, maxPips))[1]
 
-        msg = await self.dice_backend_slash(sides, dice, flat_addition)
+        msg = await dice_backend_slash(sides, dice, flat_addition)
         if note != '':
             note = f'*{note}*\n'
             msg = note + msg
 
         if private:
-            sent = await inter.reply(msg, ephemeral = private)
+            sent = await inter.response.send_message(msg, ephemeral = private)
         else:
-            button = [ActionRow(Button(
-                style = ButtonStyle.gray,
-                label = 'Roll Again',
-                custom_id = 'reroll'
-            ))]
+            buttons = RerollButton(timeout = self.timeout,
+                                   note = note, sides = sides, dice = dice, flat_addition = flat_addition)
+            await inter.response.send_message(msg, view = buttons)
 
-            # prefix = inter.author.display_name + ' rolled '
-            sent = await inter.reply(msg, components = button)
+class RerollButton(View):
+    def __init__(self, *, timeout : int = 180,
+                 note = None, sides = None, dice = None, flat_addition = None,
+                 mc = None):
+        super().__init__(timeout = timeout)
+        self.note = note
+        self.sides = sides
+        self.dice = dice
+        self.flat_addition = flat_addition
+        self.mc = mc
 
-            on_click = sent.create_click_listener(timeout = self.timeout)
-            button_owner = inter.author.id
+    @button(label = "Roll Again", style = ButtonStyle.gray)
+    async def reroll_button(self, interaction : Interaction, button : Button):
+        if self.mc:
+            extra = await dice_backend(self.mc)
+            await interaction.send(extra)
+        else:
+            extra = await dice_backend_slash(self.sides, self.dice, self.flat_addition)
+            await interaction.response.send_message(extra)
 
-            @on_click.matching_id('reroll')
-            async def on_matching_reroll(inter):
-                if inter.author.id == button_owner:
-                    first = inter.message.content.index('\n')+1 if note else 0
-                    if inter.message.content.count('\n') > 6:
-                        first = inter.message.content.index('\n', first)
-                        final = inter.message.content[inter.message.content.index('\n', first+1)+1:]
-                    else:
-                        if note:
-                            final = inter.message.content[first:]
-                        else:
-                            final = inter.message.content
-                    extra = await self.dice_backend_slash(sides, dice, flat_addition)
-                    final = note + final + '\n' + extra
-                    await inter.reply(content = final, type = ResponseType.UpdateMessage)
-                else:
-                    await inter.create_response(content = 'The Reroll button is reserved '
-                                                          'for whoever ran the original message.',
-                                                ephemeral = True)
 
-            @on_click.timeout
-            async def on_timeout():
-                await sent.edit(components = [])
-
-def setup(bot):
-    bot.add_cog(Dice(bot))
+async def setup(bot):
+    await bot.add_cog(Dice(bot))
